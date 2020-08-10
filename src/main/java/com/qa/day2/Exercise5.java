53eea3b6a45c6bf16cb2368168dd2ea7f749e00e
Nathan Orsmond <orsmondn@hotmail.com>
2020-08-10 16:42:05 +0100

    Created and started editing Exercise5 file.

---
+++ src/main/java/com/qa/day2/Exercise5.java
@@ -0,0 +1,15 @@
+package com.qa.day2;
+
+public class Exercise5 {
+
+    public static void main(String[] args) {
+        uniqueSum(2, 2, 5);
+        uniqueSum(5, 2, 5);
+        uniqueSum(2, 3, 5);
+    }
+
+    public static void uniqueSum(int a, int b, int c) {
+
+    }
+
+}
