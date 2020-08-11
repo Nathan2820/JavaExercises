781d2375f8b89c5441ea9da6a0840532e13483c2
Nathan Orsmond <orsmondn@hotmail.com>
2020-08-11 11:36:32 +0100

    Made changes to Exercise1 file.

--- src/main/java/com/qa/day3/Exercise1.java
+++ src/main/java/com/qa/day3/Exercise1.java
@@ -3,9 +3,12 @@ package com.qa.day3;
 public class Exercise1 {
 
     public static void main(String[] args) {
-
+        methodOne();
     }
 
-
-
+    public static void methodOne() {
+        for (int A = 100; A < 200; A++) {
+            System.out.println(A);
+        }
+    }
 }
