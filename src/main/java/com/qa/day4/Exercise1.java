f6fafacef005483e5cc8a885fe5a5ef0f3cc5068
Nathan Orsmond <orsmondn@hotmail.com>
2020-08-12 11:38:04 +0100

    Edited Exercise1 file using peer programming techniques.

--- src/main/java/com/qa/day4/Exercise1.java
+++ src/main/java/com/qa/day4/Exercise1.java
@@ -2,12 +2,27 @@ package com.qa.day4;
 
 public class Exercise1 {
 
-    public static void main(String[] args) {
-        methodOne();
+    public static void main (String[] args) {
+        method1(18);
     }
 
-    public static void methodOne() {
-
+    public static void method1(int startingValue) {
+        int count = 2;
+        while (startingValue > 1) {
+            System.out.println(startingValue);
+            if (startingValue % count == 0) {
+                startingValue = startingValue / count;
+                count = count + 1;
+                if (startingValue == 1) {
+                    System.out.println(startingValue);
+                    System.out.println((count - 1) + "!");
+                    break;
+                }
+            } else {
+                System.out.println("None!");
+                break;
+            }
+        }
     }
 
 }
