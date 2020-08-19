b5d2dcd76eaa482e255d394a80fa38bb98309658
Nathan Orsmond <orsmondn@hotmail.com>
2020-08-19 15:40:56 +0100

    Finished all tasks within StreamsExercise file.

--- src/main/java/com/qa/day8/StreamsExercise.java
+++ src/main/java/com/qa/day8/StreamsExercise.java
@@ -7,6 +7,8 @@ public class StreamsExercise {
     public static void main(String[] args) {
         StreamsExercise streams = new StreamsExercise();
         streams.loopThroughNames();
+        streams.reduceList();
+        streams.streamExamples();
     }
 
     public List<String> loopThroughNames() {
@@ -15,9 +17,59 @@ public class StreamsExercise {
                 names.stream()
                     .map(str -> ("Hello " + str))
                     .collect(Collectors.toList());
-                helloNames
-                    .forEach(System.out::println);
+        helloNames.forEach(System.out::println);
         return helloNames;
+    }
+
+    public int reduceList() {
+        List<Integer> numbers = Arrays.asList(3,4,7,8,12);
+        int result =
+                numbers.stream()
+                    .reduce((a,b) -> a*b)
+                    .get();
+        System.out.println(result);
+        return result;
+    }
+
+    public int streamExamples() {
+        List<Integer> numbers = Arrays.asList(12,8,10,17,5,31);
+        int max =
+                numbers.stream()
+                    .max(Integer::compare)
+                    .get();
+        System.out.println(max + "\n");
+        int min =
+                numbers.stream()
+                    .min(Integer::compare)
+                    .get();
+        System.out.println(min + "\n");
+        List<Integer> evens =
+                numbers.stream()
+                    .filter(x -> x % 2 == 0)
+                    .collect(Collectors.toList());
+        evens.forEach(System.out::println);
+        System.out.println();
+        List<Integer> odds =
+                numbers.stream()
+                        .filter(x -> x % 2 != 0)
+                        .collect(Collectors.toList());
+        odds.forEach(System.out::println);
+        System.out.println();
+        int sum =
+                numbers.stream()
+                    .reduce(0, Integer::sum);
+        System.out.println(sum + "\n");
+        List<Integer> oddsSquared =
+                numbers.stream()
+                    .map(x -> x*x)
+                    .filter(x -> x % 2 != 0)
+                    .collect(Collectors.toList());
+        int minValue =
+                oddsSquared.stream()
+                    .min(Integer::compare)
+                    .get();
+        System.out.println(minValue + "\n");
+        return max;
     }
 
 }
